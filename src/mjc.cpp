f7ede823d18cdc5d73139137e7af38ac75fea939
Henrique Marques <henrique13210@gmail.com>
2025-06-11 21:10:47 -0400

    added tests

--- src/mjc.cpp
+++ src/mjc.cpp
@@ -1,39 +1,62 @@
 #include "parser.h"
 
 #include <fstream>
+#include <filesystem>
 
 using std::cout;
 std::ifstream fin;
 
-void
-scannerTest()
+void fullTest()
 {
-    auto globalST = new SymbolTable();
-    auto scanner = new Scanner(globalST);
-
-    Token *token;
-    do
+    for (auto const& dir_entry : std::filesystem::directory_iterator{"tests"})
     {
-        try
-        {
-            token = scanner->scan();
-        }
-        catch(Error err) {
-            err.what();
-            return;
-        }
-#ifdef _DEBUG
-        std::cout << token->lexeme << ' ';
-#endif
-    } while (token->type != Types::END_OF_FILE);
+        if (dir_entry.is_directory()) continue;
+        fin.open(dir_entry.path());
 
-    delete globalST;
-    delete scanner;
+        auto globalST = new SymbolTable();
+        auto scanner = new Scanner(globalST);
+
+        Token* token;
+        do
+        {
+            try
+            {
+                token = scanner->scan();
+            }
+            catch (Error err)
+            {
+                err.what();
+                return;
+            }
+#ifdef _DEBUG
+            std::cout << token->lexeme << '\n';
+#endif
+        } while (token->type != Types::END_OF_FILE);
+
+        delete globalST;
+        delete scanner;
+        fin.close();
+    }
+    exit(0);
+
+    /*
+    try
+    {
+        Parser parser;
+        parser.run();
+    }
+    catch (Error err)
+    {
+        err.what();
+    }
+    */
 }
 
 int
 main(int argc, char *argv[])
 {
+    fullTest();
+
     // Verifica se foi executado corretamente
     // Essa main espera receber o nome do arquivo a ser
     // executado na linha de comando.
@@ -49,8 +72,6 @@ main(int argc, char *argv[])
         cout << "Não foi possível abrir o arquivo\n";
         return 1;
     }
-
-    scannerTest();
     
     /*
     try
